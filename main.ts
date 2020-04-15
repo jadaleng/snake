a83865d450991d0af4b2b22cb37848ae29a6dec6
jadaleng <jadaleng@me.com>
2020-04-15 16:05:11 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 let bg_red = 0;
 let bg_green = 0;
 let bg_blue = 0;
+let hue = 0;
+let display = GAME_ZIP64.createZIP64Display();
 
 function hueToBgRgb(hue: number) {
     function hue2rgb(t: number) {
@@ -13,12 +15,15 @@ function hueToBgRgb(hue: number) {
     }
 
     bg_red = hue2rgb(hue + 1 / 3) * 255;
-    bg_green = hue2rgb(hue);
-    bg_blue = hue2rgb(hue - 1 / 3);
+    bg_green = hue2rgb(hue) * 255;
+    bg_blue = hue2rgb(hue - 1 / 3) * 255;
 }
 
 basic.forever(function () {
-    let display = GAME_ZIP64.createZIP64Display()
+   
 
     display.showColor(GAME_ZIP64.rgb(bg_red, bg_green, bg_blue))
+    basic.pause(100)
+    hue++;
+    hue = hue%360;
 })
