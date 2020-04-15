40755deb4cc0494a6be34330e8d2512c71c6362e
jadaleng <jadaleng@me.com>
2020-04-15 18:44:57 +0200

    Update main.ts, test.ts

--- main.ts
+++ main.ts
@@ -6,36 +6,40 @@
 namespace Game64Tools {
 
     /**
-     * A function that returns a non-void argument generates a reporter block (oval shape).
+     * A hue, saturation and luminance to RGB conversion function
      */
     //% block
-    export function hueSatLightToColor(h : number, s : number, l : number): number {
-        let r, g, b;
+    export function hueSatLightToColor(hue : number, sat : number, lum : number): number {
 
-        if (s == 0) {
-            r = g = b = l; // achromatic
+        let v : number;
+        let r,g,b : number;
+
+        v = (lum < 128) ? (lum * (256 + sat)) >> 8 : (((lum + sat) << 8) - lum * sat) >> 8;
+        if (v <= 0) {
+            r = g = b = 0;
         } else {
-            function hue2rgb(p : number, q : number, t : number)
-            {
-                if (t < 0) t += 1;
-                if (t > 1) t -= 1;
-                if (t < 1 / 6) return p + (q - p) * 6 * t;
-                if (t < 1 / 2) return q;
-                if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-                return p;
+            let m : number;
+            let sextant : number;
+            let fract, vsf, mid1, mid2 : number;
+
+            m = lum + lum - v;
+            hue *= 6;
+            sextant = hue >> 8;
+            fract = hue - (sextant << 8);
+            vsf = v * fract * (v - m) / v >> 8;
+            mid1 = m + vsf;
+            mid2 = v - vsf;
+            switch (sextant) {
+                case 0: r = v; g = mid1; b = m; break;
+                case 1: r = mid2; g = v; b = m; break;
+                case 2: r = m; g = v; b = mid1; break;
+                case 3: r = m; g = mid2; b = v; break;
+                case 4: r = mid1; g = m; b = v; break;
+                case 5: r = v; g = m; b = mid2; break;
             }
-
-            let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-            let p = 2 * l - q;
-
-            r = hue2rgb(p, q, h + 1 / 3);
-            g = hue2rgb(p, q, h);
-            b = hue2rgb(p, q, h - 1 / 3);
         }
 
-        return GAME_ZIP64.rgb(Math.max(0, Math.min(Math.round(r * 255), 255)) 
-            ,Math.max(0, Math.min(Math.round(g * 255), 255)) 
-            ,Math.max(0, Math.min(Math.round(b * 255), 255)) );
+        return GAME_ZIP64.rgb(r,g,b);
     }
 };
 
