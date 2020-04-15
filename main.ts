e67ee5bc81ad89f15bc10e9810a6692565f1f806
jadaleng <jadaleng@me.com>
2020-04-15 16:35:55 +0200

    Attempt at a simple extension

--- main.ts
+++ main.ts
@@ -1,29 +1,45 @@
-let bg_red = 0;
-let bg_green = 0;
-let bg_blue = 0;
-let hue = 0;
-let display = GAME_ZIP64.createZIP64Display();
 
-function hueToBgRgb(hue: number) {
-    function hue2rgb(t: number) {
-        if (t < 0) t += 1;
-        if (t > 1) t -= 1;
-        if (t < 1 / 6) return 6 * t;
-        if (t < 1 / 2) return 1;
-        if (t < 2 / 3) return (2 / 3 - t) * 6;
-        return 1;
+
+namespace Game64Tools {
+
+    function hslToRgb(h : number, s : number, l : number) : Array<number> {
+        let r, g, b;
+
+        if (s == 0) {
+            r = g = b = l; // achromatic
+        } else {
+            function hue2rgb(p : number, q : number, t : number)
+            {
+                if (t < 0) t += 1;
+                if (t > 1) t -= 1;
+                if (t < 1 / 6) return p + (q - p) * 6 * t;
+                if (t < 1 / 2) return q;
+                if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+                return p;
+            }
+
+            let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            let p = 2 * l - q;
+
+            r = hue2rgb(p, q, h + 1 / 3);
+            g = hue2rgb(p, q, h);
+            b = hue2rgb(p, q, h - 1 / 3);
+        }
+
+        return [
+            Math.max(0, Math.min(Math.round(r * 255), 255)) 
+            ,Math.max(0, Math.min(Math.round(g * 255), 255)) 
+            ,Math.max(0, Math.min(Math.round(b * 255), 255)) 
+        ];
     }
 
-    bg_red = hue2rgb(hue + 1 / 3) * 255;
-    bg_green = hue2rgb(hue) * 255;
-    bg_blue = hue2rgb(hue - 1 / 3) * 255;
-}
+    /**
+     * A function that returns a non-void argument generates a reporter block (oval shape).
+     */
+    //% block
+    export function hueSatLightToColor(h : number, s : number, l : number): number {
+        let [r,g,b] = hslToRgb(h, s, l);
+        return GAME_ZIP64.rgb(r, g, b);
+    }
+};
 
-basic.forever(function () {
-   
-
-    display.showColor(GAME_ZIP64.rgb(bg_red, bg_green, bg_blue))
-    basic.pause(100)
-    hue++;
-    hue = hue%360;
-})
