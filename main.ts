0f93245ca6591fe8246d4a62b77b26a5eef9edd3
jadaleng <jadaleng@me.com>
2020-04-15 15:58:14 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,24 @@
-function draw_snake () {
-    display.clear()
-    fill_screen_with_color(4, 0, 2)
-    display.setMatrixColor(snake_x, snake_y, GAME_ZIP64.colors(ZipLedColors.Violet))
-    display.show()
-}
-function fill_screen_with_color (red: number, green: number, blue: number) {
-    for (let x = 0; x <= 8; x++) {
-        for (let y = 0; y <= 8; y++) {
-            display.setMatrixColor(x, y, GAME_ZIP64.rgb(red, green, blue))
-        }
+let bg_red = 0;
+let bg_green = 0;
+let bg_blue = 0;
+
+function hueToBgRgb(hue: number) {
+    function hue2rgb(t: number) {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1 / 6) return 6 * t;
+        if (t < 1 / 2) return 1;
+        if (t < 2 / 3) return (2 / 3 - t) * 6;
+        return 1;
     }
+
+    bg_red = hue2rgb(hue + 1 / 3) * 255;
+    bg_green = hue2rgb(hue);
+    bg_blue = hue2rgb(hue - 1 / 3);
 }
-GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Right, GAME_ZIP64.ZIP64ButtonEvents.Down, function () {
-    snake_x += 1
-    draw_snake()
+
+basic.forever(function () {
+    let display = GAME_ZIP64.createZIP64Display()
+
+    display.showColor(GAME_ZIP64.rgb(bg_red, bg_green, bg_blue))
 })
-GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Left, GAME_ZIP64.ZIP64ButtonEvents.Down, function () {
-    snake_x += -1
-    draw_snake()
-})
-GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Up, GAME_ZIP64.ZIP64ButtonEvents.Down, function () {
-    snake_y += -1
-    draw_snake()
-})
-GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Down, GAME_ZIP64.ZIP64ButtonEvents.Down, function () {
-    snake_y += 1
-    draw_snake()
-})
-let snake_y = 0
-let snake_x = 0
-let display: GAME_ZIP64.ZIP64Display = null
-display = GAME_ZIP64.createZIP64Display()
-snake_x = 4
-snake_y = 5
-draw_snake()
